4a3ca2d7b4726f14727c546ed18922ed83bf4ce5
hiraicn <hiraicn@gmail.com>
2017-04-28 12:53:02 +0800

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^index/$', views.index),
+    url(r'^article/(?P<article_id>[0-9]+)$', views.article_page, name='article_page'),
+]
