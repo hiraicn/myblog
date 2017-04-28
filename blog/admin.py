472d7d528ccbc042e536f024e8e29f3b76380f78
hiraicn <hiraicn@gmail.com>
2017-04-29 00:47:05 +0800

    finish

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-
-from . import models
+from .models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'content','pub_time')
     list_filter = ('pub_time', )
 
-admin.site.register(models.Article, ArticleAdmin)
+admin.site.register(Article, ArticleAdmin)
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
 from . import models
-
-# Create your views here.
 
 def index(request):
     articles = models.Article.objects.all()
