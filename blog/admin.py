2acfc7f78ba705485ab91a2466ccf686636d82c1
hiraicn <hiraicn@gmail.com>
2017-04-29 00:12:45 +0800

    教程完成

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+
 from . import models
 
-admin.site.register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content','pub_time')
+    list_filter = ('pub_time', )
 
-# Register your models here.
+admin.site.register(models.Article, ArticleAdmin)
+
+
